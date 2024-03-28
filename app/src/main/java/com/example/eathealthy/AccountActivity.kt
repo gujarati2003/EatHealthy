7cb7309118546b50715e9c7285cce1f789676e4c
Amy Patel <126843012+gujarati2003@users.noreply.github.com>
2024-03-28 11:29:38 -0500

    delete imports

--- app/src/main/java/com/example/eathealthy/AccountActivity.kt
+++ app/src/main/java/com/example/eathealthy/AccountActivity.kt
@@ -1,14 +1,8 @@
 package com.example.eathealthy
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.eathealthy.databinding.ActivityAccountBinding
-import com.example.eathealthy.databinding.ActivityLoginBinding
 
 class AccountActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAccountBinding
